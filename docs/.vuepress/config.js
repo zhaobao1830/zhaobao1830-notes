8e66b226b2bbf172f08ffbaad63d0315907960f2
zhaobao1830 <zgt..5219438>
2021-02-05 15:19:13 +0800

    基础配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,56 @@
-const pkg = require('../../package.json')
-
 module.exports = {
-  title: pkg.name,
-  description: 'vue插件文档',
+  title: '技术博客',
+  description: '前后端常用知识、踩坑记录、封装的插件、随手笔记等',
   dest: 'dist', // 设置输出目录
+  // 这里与https://zhaobao1830.github.io/zhaobao1830-notes/中的zhaobao1830-notes一致，
+  // 如果网站是https://zhaobao1830.github.io/，那base值就是/
   base: '/zhaobao1830-notes/',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '技术博客',
+      description: '前后端常用知识、踩坑记录、封装的插件、随手笔记等'
+    }
+  },
   themeConfig: {
     nav: [
-      { text: '主页', link: '/' }
+      { text: '主页', link: '/' },
+      {
+        text: 'Vue',
+        link: '/vue/'
+      },
+      {
+        text: 'React',
+        link: '/react/'
+      },
+      {
+        text: 'Java',
+        link: '/java/'
+      },
+      {
+        text: '外网链接',
+        items: [
+          {
+            text: 'helloworld',
+            link: 'https://www.helloworld.net/'
+          }
+        ]
+      }
     ],
-    sidebarDepth: 1
+    sidebar: [
+      {
+        title: 'Vue',
+        path: '/vue/'
+      },
+      {
+        title: 'React',
+        path: '/react/'
+      },
+      {
+        title: 'Java',
+        path: '/java/'
+      }
+    ]
   },
   plugins: [
     // 回到顶部
--- docs/README.md
+++ docs/README.md
@@ -4,9 +4,7 @@ heroImage: /home.webp
 actionText: 快速上手 →
 actionLink: /guide/introduction
 features:
-- title: 配置化页面
-  details: 对常用页面组件封装,配置化
-- title: 简单高效
-  details: 对属性配置简化
-- title: 轻松上手
-  details: 兼容官方api,易上手,易扩展。
+- title: Vue/React
+  details: Vue/React知识点和基础组件封装
+- title: Java
+  details: Java知识点
