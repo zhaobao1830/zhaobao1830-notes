2e23257ce2d28de96a0ff51017a8710552c6152b
zhaobao1830 <373098777@qq.com>
2022-06-30 18:07:28 +0800

    新加vue/no-mutating-props错误

--- docs/.vuepress/config/stage.ts
+++ docs/.vuepress/config/stage.ts
@@ -161,7 +161,8 @@ const takeNotesSidebar = [
       '/takeNotes/vue/07',
       '/takeNotes/vue/08',
       '/takeNotes/vue/09',
-      '/takeNotes/vue/10'
+      '/takeNotes/vue/10',
+      '/takeNotes/vue/11'
     ]
   },
   {
