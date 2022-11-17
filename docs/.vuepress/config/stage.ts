0d2aa20ad8f0fb81f3ba981f280760c28a268de5
zhaobao1830 <373098777@qq.com>
2022-11-17 17:06:18 +0800

    新加splice笔记

--- docs/.vuepress/config/stage.ts
+++ docs/.vuepress/config/stage.ts
@@ -179,6 +179,12 @@ const takeNotesSidebar = [
     ]
   },
   {
+    text: 'js',
+    children: [
+      '/takeNotes/js/01'
+    ]
+  },
+  {
     text: 'vite',
     children: [
       '/takeNotes/vite/01',
