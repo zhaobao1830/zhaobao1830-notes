c3c661311fbdd113e939cbd2c5e3eaca7640ddc1
zhaobao1830 <373098777@qq.com>
2023-02-12 09:20:46 +0800

    新加docsearch插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -202,9 +202,52 @@ export default {
   }),
   plugins: [
     docsearchPlugin({
-      appId: "G0W1RASOD3",
-      apiKey: "c4b8fb7cfc92e31087d32ec6ac30d083",
-      indexName: "zhaobao1830"
+      appId: 'G0W1RASOD3',
+      apiKey: '473cca412f78e6a7233a3400d0ef2c54',
+      indexName: 'zhaobao1830',
+      locales: {
+        '/': {
+          placeholder: '搜索',
+          translations: {
+            button: {
+              buttonText: '搜索',
+              buttonAriaLabel: '搜索'
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消'
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除'
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接'
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者'
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈'
+              }
+            }
+          }
+        }
+      }
     })
   ]
 }
