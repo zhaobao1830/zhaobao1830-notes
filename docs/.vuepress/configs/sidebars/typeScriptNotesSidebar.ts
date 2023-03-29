93917ca37d8a8f1fc520433198eca0db995097e2
zhaobao1830 <373098777@qq.com>
2023-03-29 08:53:41 +0800

    新加TS文档

--- docs/.vuepress/configs/sidebars/typeScriptNotesSidebar.ts
+++ docs/.vuepress/configs/sidebars/typeScriptNotesSidebar.ts
@@ -3,7 +3,8 @@ export const typeScriptNotesSidebar = [
     text: 'typeScript笔记',
     children: [
       '/typeScriptNotes/01',
-      '/typeScriptNotes/02'
+      '/typeScriptNotes/02',
+      '/typeScriptNotes/03'
     ]
   }
 ]
