1eb0e1fed0c3f3fb901c46597862534acb21cb2c
zhaobao1830 <373098777@qq.com>
2023-05-05 17:05:16 +0800

    新加mybatis传参方法文档

--- docs/.vuepress/configs/sidebars/mybatisKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/mybatisKnowledgeSidebar.ts
@@ -2,7 +2,8 @@ export const mybatisKnowledgeSidebar = [
   {
     text: 'mybatis知识点',
     children: [
-      '/mybatisKnowledge/01'
+      '/mybatisKnowledge/01',
+      '/mybatisKnowledge/02'
     ]
   }
 ]
