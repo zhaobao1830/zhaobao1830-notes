1464b63f89bfd4f9bc98f266e96c6aee93eb1a6c
zhaobao1830 <373098777@qq.com>
2023-05-22 10:40:45 +0800

    新加scroll-view文档

--- docs/.vuepress/configs/sidebars/miniProgramNotesSidebar.ts
+++ docs/.vuepress/configs/sidebars/miniProgramNotesSidebar.ts
@@ -3,7 +3,9 @@ export const miniProgramNotesSidebar = [
     text: '小程序笔记',
     children: [
       '/miniProgramNotes/01',
-      '/miniProgramNotes/02'
+      '/miniProgramNotes/02',
+      '/miniProgramNotes/03',
+      '/miniProgramNotes/04'
     ]
   }
 ]
