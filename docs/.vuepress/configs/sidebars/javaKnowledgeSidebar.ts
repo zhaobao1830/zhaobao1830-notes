172cf8fa0b3adc7445a90f9f442641be433c82b8
zhaobao1830 <373098777@qq.com>
2023-06-06 17:49:11 +0800

    根据目录结构自动生成路由前缀

--- docs/.vuepress/configs/sidebars/javaKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/javaKnowledgeSidebar.ts
@@ -7,7 +7,8 @@ export const javaKnowledgeSidebar = [
       '/javaKnowledge/03',
       '/javaKnowledge/04',
       '/javaKnowledge/05',
-      '/javaKnowledge/06'
+      '/javaKnowledge/06',
+      '/javaKnowledge/07'
     ]
   }
 ]
