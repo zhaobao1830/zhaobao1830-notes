50678de2375c6c661bd7ac5c1b990c27adee582d
zhaobao1830 <373098777@qq.com>
2023-06-13 11:27:07 +0800

    TCP、UDP和HTTP的区别

--- docs/.vuepress/configs/sidebars/browserKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/browserKnowledgeSidebar.ts
@@ -3,7 +3,8 @@ export const browserKnowledgeSidebar = [
     text: '浏览器知识点',
     children: [
       '/browserKnowledge/01',
-      '/browserKnowledge/02'
+      '/browserKnowledge/02',
+      '/browserKnowledge/03'
     ]
   }
 ]
