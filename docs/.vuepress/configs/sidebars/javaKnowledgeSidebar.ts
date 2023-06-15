f4c32741edfc7c515f8321dde681b20817341f14
zhaobao1830 <373098777@qq.com>
2023-06-15 16:27:16 +0800

    新加参数校验笔记

--- docs/.vuepress/configs/sidebars/javaKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/javaKnowledgeSidebar.ts
@@ -10,7 +10,7 @@ export const javaKnowledgeSidebar = [
       '/javaKnowledge/06',
       '/javaKnowledge/07',
       '/javaKnowledge/08',
-      '/javaKnowledge/08'
+      '/javaKnowledge/09'
     ]
   }
 ]
