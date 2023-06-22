9ec16aa0d3c52a98fa44f028327644adc4f588f5
zhaobao1830 <373098777@qq.com>
2023-06-22 19:07:11 +0800

    Vue组件之间的通信方式有哪些？

--- docs/.vuepress/configs/sidebars/vueKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/vueKnowledgeSidebar.ts
@@ -12,7 +12,8 @@ export const vueKnowledgeSidebar = [
       '/vueKnowledge/08',
       '/vueKnowledge/09',
       '/vueKnowledge/10',
-      '/vueKnowledge/11'
+      '/vueKnowledge/11',
+      '/vueKnowledge/12'
     ]
   }
 ]
