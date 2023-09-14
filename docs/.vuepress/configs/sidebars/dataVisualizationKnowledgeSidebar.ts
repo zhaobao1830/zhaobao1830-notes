fd25023a83f0b86c171c50aef731801176e535c7
zhaobao1830 <373098777@qq.com>
2023-09-14 15:49:17 +0800

    新加canvas文档

--- docs/.vuepress/configs/sidebars/dataVisualizationKnowledgeSidebar.ts
+++ docs/.vuepress/configs/sidebars/dataVisualizationKnowledgeSidebar.ts
@@ -3,6 +3,7 @@ export const dataVisualizationKnowledgeSidebar = [
     text: '数据可视化知识点',
     children: [
       '/dataVisualizationKnowledge/01',
+      '/dataVisualizationKnowledge/03',
       '/dataVisualizationKnowledge/02'
     ]
   }
