b283b7fe77101d797b6a9f9512f5511697995f45
zhaobao1830 <373098777@qq.com>
2024-02-06 09:36:59 +0800

    新加关于IDEA的三个文档

--- docs/.vitepress/configs/sidebars/jetBrainsNotesSidebar.ts
+++ docs/.vitepress/configs/sidebars/jetBrainsNotesSidebar.ts
@@ -13,6 +13,18 @@ export const jetBrainsNotesSidebar = [
       {
         text: '开发uniapp项目时，rpx报错',
         link: '/jetBrainsNotes/03'
+      },
+      {
+        text: '开IDEA自定义注释@Author、@Date、@Description',
+        link: '/jetBrainsNotes/04'
+      },
+      {
+        text: '自定义的@Author、@Date、@Description注释下有波浪线',
+        link: '/jetBrainsNotes/05'
+      },
+      {
+        text: 'statement 或 DELIMITER，得到 \'id\'',
+        link: '/jetBrainsNotes/06'
       }
     ]
   }
