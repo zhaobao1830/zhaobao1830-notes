5faa8f4252aaa6d530b0dab585478489255f8958
zhaobao1830 <373098777@qq.com>
2024-02-28 15:20:09 +0800

    新加NoSql文档

--- docs/.vitepress/configs/sidebars/dataBaseKnowledgeSidebar.ts
+++ docs/.vitepress/configs/sidebars/dataBaseKnowledgeSidebar.ts
@@ -3,13 +3,17 @@ export const dataBaseKnowledgeSidebar = [
     text: '数据库知识点',
     items: [
       {
-        text: 'mySql和mongodb的区别',
+        text: 'NoSql',
         link: '/dataBaseKnowledge/01'
       },
       {
         text: 'HikariCP',
         link: '/dataBaseKnowledge/02'
-      }
+      },
+      {
+        text: 'mySql和mongodb的区别',
+        link: '/dataBaseKnowledge/03'
+      },
     ]
   }
 ]
