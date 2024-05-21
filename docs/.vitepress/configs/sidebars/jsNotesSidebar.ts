7d2fd6788e65d1f8304b7e53cc44fb2ef447539c
zhaobao1830 <373098777@qq.com>
2024-05-21 10:26:40 +0800

    新加console使用方式文章

--- docs/.vitepress/configs/sidebars/jsNotesSidebar.ts
+++ docs/.vitepress/configs/sidebars/jsNotesSidebar.ts
@@ -8,31 +8,35 @@ export const jsNotesSidebar = [
       },
       {
         text: '使用new map存取删数据',
-        link: '/jsNotes/01'
+        link: '/jsNotes/02'
       },
       {
         text: '使用node-qrcode生成二维码',
-        link: '/jsNotes/01'
+        link: '/jsNotes/03'
       },
       {
         text: '使用clipboard.js实现拷贝文本',
-        link: '/jsNotes/01'
+        link: '/jsNotes/04'
       },
       {
         text: '现快捷键',
-        link: '/jsNotes/01'
+        link: '/jsNotes/05'
       },
       {
         text: '使用cropperjs实现图片裁剪',
-        link: '/jsNotes/01'
+        link: '/jsNotes/06'
       },
       {
         text: 'js里使用单例模式',
-        link: '/jsNotes/01'
+        link: '/jsNotes/07'
       },
       {
         text: 'js中数值计算封装',
-        link: '/jsNotes/01'
+        link: '/jsNotes/08'
+      },
+      {
+        text: 'console使用方式',
+        link: '/jsNotes/09'
       }
     ]
   }
