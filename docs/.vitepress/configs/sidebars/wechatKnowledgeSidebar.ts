6fdb78e561de5392afc314e56a90c6589dea0b49
zhaobao1830 <373098777@qq.com>
2024-05-31 11:04:51 +0800

    新加微信公众号--JSAPI支付博客

--- docs/.vitepress/configs/sidebars/wechatKnowledgeSidebar.ts
+++ docs/.vitepress/configs/sidebars/wechatKnowledgeSidebar.ts
@@ -5,6 +5,10 @@ export const wechatKnowledgeSidebar = [
       {
         text: '微信公众号网页授权：获取用户openid和基本信息',
         link: '/wechatKnowledge/01'
+      },
+      {
+        text: '微信公众号--JSAPI支付',
+        link: '/wechatKnowledge/02'
       }
     ]
   }
