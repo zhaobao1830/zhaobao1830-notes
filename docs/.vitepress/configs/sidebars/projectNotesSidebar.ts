403c10e63c2301cb1acbdcf56a2f2829699dd939
zhaobao1830 <373098777@qq.com>
2026-03-05 09:41:07 +0800

    新加charge（收费）项目中的字典功能文档

--- docs/.vitepress/configs/sidebars/projectNotesSidebar.ts
+++ docs/.vitepress/configs/sidebars/projectNotesSidebar.ts
@@ -5,6 +5,10 @@ export const projectNotesSidebar = [
       {
         text: '参保地医保区划',
         link: '/projectNotes/01'
+      },
+      {
+        text: 'charge（收费）项目中的字典功能',
+        link: '/projectNotes/02'
       }
     ]
   }
