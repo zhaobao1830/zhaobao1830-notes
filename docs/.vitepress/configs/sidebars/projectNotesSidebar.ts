d210fd777865f720d743ccf09cb1eda6c54263ba
zhaobao1830 <373098777@qq.com>
2026-03-13 15:05:43 +0800

    新加charge（收费）项目中的诊断选择功能文档

--- docs/.vitepress/configs/sidebars/projectNotesSidebar.ts
+++ docs/.vitepress/configs/sidebars/projectNotesSidebar.ts
@@ -3,12 +3,16 @@ export const projectNotesSidebar = [
     text: '项目笔记',
     items: [
       {
-        text: '参保地医保区划',
+        text: 'charge（收费）项目中的参保地医保区划选择功能',
         link: '/projectNotes/01'
       },
       {
         text: 'charge（收费）项目中的字典功能',
         link: '/projectNotes/02'
+      },
+      {
+        text: 'charge（收费）项目中的诊断选择功能',
+        link: '/projectNotes/03'
       }
     ]
   }
